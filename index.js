ead3b15497d46e75d52bd8b09f9ed9d924b104ae
Su Wang <wangsu01@baidu.com>
2014-11-17 20:34:31 +0800

    补充注释,重命名RandomOf为RandomIterator,Rolling为RollingIterator

--- index.js
+++ index.js
@@ -1,92 +1,214 @@
 /**
- * New node file
+ * 随机工具. 用于生成字符串,数字,或在一组对像中随机取值.
  */
 
+/**
+ * @inner
+ */
 var defaultCharts = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-var random = function(max,min,q){
-    
+/**
+ * 
+ * 生成一个区间内的随机数
+ * 
+ * @param {number} max 最大值
+ * @param {number} [min] 最小值
+ * @returns {number} this value between the max and min;
+ */
+var random = function(max, min) {
+
     min = ~~min || 0;
     max = ~~max || 0;
-    
-    //min, max is not 0
-    if(min && max){
+
+    // min, max is not 0
+    if (min && max) {
         max = max - min + 1;
         return ~~(Math.random() * max) + min;
-    }else if(max){
+    } else if (max) {
         // faster the 0 - max;
         return ~~(Math.random() * max);
     }
-    
+
     // faster return 0;
     return min;
 };
 
 /**
  * 生成包含[a-zA-Z0-9]的指定长度的随机字符串
+ * 
+ * @param {number} [len] 指定长度,默认为 10;
+ * @return {string} this string include the [a-z0-9] and length is len
+ */
+var randomStr = function(len) {
+    var str = '';
+    len = len || 10;
+    while (str.length < len) {
+        str += defaultCharts[random(36)];
+    }
+
+    return str;
+};
+
+/**
+ * 生成包含charts指定集合内字符的指定长度的随机字符串
+ * 
  * @param {number} len 指定长度,默认为 10;
- * @return {string} 生成的指定长度字符串
- */ 
-var randomStr = function(_len){
-    var str = '' , len = _len || 10;
-    for(; str.length < len ; str += defaultCharts[random(36)]);
-    return str;
-};
+ * @param {charts} [charts] 指定长度,默认为 10;
+ * @return {string}
+ */
+var randomStrLimit = function(len, charts) {
+    var str = ''; var chartsLen;
 
-var randomStrLimit = function(_len,charts){
-    var str = '' , len = _len || 10, chartsLen;
+    len = len || 10;
     charts = charts || defaultCharts;
-    chartsLen = charts.length;
-    for(; str.length < len ; str += charts[random(chartsLen)]);
-    return str;
     
+    chartsLen = charts.length;
+
+    while (str.length < len) {
+        str += charts[random(chartsLen)];
+    }
+
+    return str;
 };
 
-var RandomOf = function(content){
-    
-    if(content && Array.isArray(content)){
+/**
+ * @class Iterator
+ * @params {array} content 初始化内容
+ */
+function Iterator(content){
+    this._index = 0;
+    if (content && Array.isArray(content)) {
         this._contents = content;
-    }else{
+    } else {
         this._contents = [content];
     }
-    
     this._size = this._contents.length;
-};
+}
 
-RandomOf.prototype = {
-    next : function(){
-        return this._contents[random(this._size)];
+Iterator.prototype = {
+    next: function(){
+        return this._contents[this._index++];
     },
-    push:function(item){
-        this._contents.push(item);
-        this._size = this._contents.length;
+    /**
+     * 添加一项到列表中
+     * @param item
+     * @returns {number} 新的内容长度
+     */
+    push: function(item) {
+        if(arguments.length > 0){
+            this._contents.push(item);
+            this._size = this._contents.length;
+        }
+        return this._size;
+    },
+    /**
+     * 从列表中移除一个或多个项目
+     * 
+     * @param condition {any} 
+     *      当condition为一个function时, 将执行这个function并传入列表的每一个值, 如果function返回true,将移除这个元素
+     *      当condition不是一个function,执行index,并删除匹配到的第一项.
+     *        
+     * @param forceItem {boolean} default false
+     *      当值为true时,认为condition永远是一个普通值
+     *      
+     * @returns {number} 新的内容长度
+     */
+    remove: function(condition, forceItem){
+        if(arguments.length > 0){
+            
+            if(!forceItem && item instanceof Function){
+                this._contents = this._contents.filter(function(){
+                    return item.apply(arguments);
+                });
+            }else{
+                // undefined and null maybe is good item;
+                var index = this._contents.indexOf(item);
+                if (index !== -1) {
+                    this._contents.splice(index,1);
+                }
+            }
+            
+            this._size = this._contents.length;
+            
+        }
+    },
+    /**
+     * 判断内容是否为空
+     * @returns {Boolean}
+     */
+    isEmpty:function(){
+        return this._contents.length === 0;
+    },
+    /**
+     * 是否仍有下一个元素
+     * @returns {Boolean}
+     */
+    hasNext:function(){
+        return  this._index < this._size; 
     }
 }
 
-module.exports  = {
-    /**
-     * 
-     * @param max
-     * @param min
-     * @returns {Number}
-     */
-    random:random,
-    /**
-     * quick get a random string include charts [a-z0-9];
-     */
-    getStr:randomStr,
-    /**
-     * get get a random string;
-     */
-    getStrLimit:randomStrLimit,
-    RandomOf:RandomOf
+/**
+ * 一个随机获取内容项的工具对像
+ * @class RandomIterator
+ * @extends Iterator
+ * @params {array} content 初始化内容
+ */
+var RandomIterator = function(content) {
+    Iterator.apply(this,arguments);
 };
 
-console.log(randomStr(50));
-console.log(randomStrLimit(50));
+RandomIterator.prototype = /** @lends RandomOf */{
+    __proto__:Iterator.prototype,
+    /**
+     * 随机获取下一项
+     * @returns {any}
+     */
+    next: function() {
+        
+        if(this.isEmpty()){
+            return;
+        }
+        
+        return this._contents[random(this._size)];
+    },
+    hasNext:function(){
+        return !this.isEmpty();
+    }
+};
+/**
+ * 一个轮寻获取内容项的工具对像
+ * @class RollingIterator
+ * @extends Iterator
+ * @params {array} content 初始化内容
+ */
+var RollingIterator = function(content) {
+    Iterator.apply(this,arguments);
+};
 
-var ro = new RandomOf(['aaa','bbb','cccc']);
+RollingIterator.prototype = /** @lends Rolling*/{
+    __proto__:Iterator.prototype,
+    /**
+     * 顺序获取下一项内容
+     * @returns {any}
+     */
+    next: function() {
+        
+        if(this.isEmpty()){
+            return;
+        }
+        
+        return this._contents[this._index++ % this._size];
+    },
+    hasNext:function(){
+        return !this.isEmpty();
+    }
+};
 
-for(var i=0;i<100;i++){
-    console.log(ro.next());
+module.exports = {
+    random: random,
+    getStr: randomStr,
+    getStrLimit: randomStrLimit,
+    RandomOf: RandomIterator,
+    Rolling: RollingIterator
 };
